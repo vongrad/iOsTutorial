e853237bedba3726a1c444bec42625beb710f75b
Radim Vongrej <radimvongrej@Mac-Pro.local>
2015-06-28 11:36:18 +0200

    Initial commit

---
+++ Test_Git/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test_Git
+//
+//  Created by Radim Vongrej on 28.06.15.
+//  Copyright (c) 2015 Radim Vongrej. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
